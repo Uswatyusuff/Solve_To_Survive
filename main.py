ab7beac78da548222da60e1079d81ff8b01d6fc9
uswatyusuf <omosewaeniola@gmail.com>
2025-04-05 12:28:27 +0100

    Adds background image, moster and player characters
    Adds text to track moster and player score and functionality to display it on the screen

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import pygame
-import time
-import random
+pygame.init()
 
 WIDTH, HEIGHT = 1250, 720
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Solve To Survive")
-# Load the image from the specified file path
-image = pygame.image.load('background_image.jpg')
 
+my_font = pygame.font.SysFont('Comic Sans MS', 30)
+
+# Load the image from the specified file path
+background_image = pygame.image.load('background_image.jpg')
+
+monster = pygame.image.load('monster.png')
+character = pygame.image.load('farmer_hamster.png')
+monster= pygame.transform.scale(monster, (200, 200))
+character = pygame.transform.scale(character, (200, 200))
+
+# Render the text (text, antialias, color)
+monster_level_bar = my_font.render("Monster score: 5", True, (255, 255, 255))  # White text
+character_level_bar = my_font.render("Player score: 5", True, (255, 255, 255))
+
+# Get a rect for positioning
+monster_text_rect = monster_level_bar.get_rect(center=(320, 260))  # Center of the screen
+character_text_rect = monster_level_bar.get_rect(center=(940, 260))
 
 def main():
     run = True
@@ -23,8 +37,11 @@ def main():
                     run = False
 
         # Draw the image on the screen at the top-left corner (0, 0)
-        WIN.blit(image, (0, 0))
-
+        WIN.blit(background_image, (0, 0))
+        WIN.blit(monster, (250, 400))
+        WIN.blit(character, (850, 400))
+        WIN.blit(monster_level_bar, monster_text_rect)
+        WIN.blit(character_level_bar, character_text_rect)
         # Update the display
         pygame.display.update()
     pygame.quit()
