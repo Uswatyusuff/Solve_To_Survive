1b2f99fb561f6cf46075fbc4cb3c5901e7ac6ef9
uswatyusuf <omosewaeniola@gmail.com>
2025-04-05 11:03:01 +0100

    Changes the size of the screen
    Adds background image for the game screen
    
    Adds functionality for the game to stop running when escape key is pressed

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import pygame
 import time
 import random
 
-WIDTH, HEIGHT = 500, 500
+WIDTH, HEIGHT = 1250, 720
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Solve To Survive")
-
+# Load the image from the specified file path
+image = pygame.image.load('background_image.jpg')
 
 
 def main():
@@ -16,6 +17,16 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 break
+                # Check if a key is pressed
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    run = False
+
+        # Draw the image on the screen at the top-left corner (0, 0)
+        WIN.blit(image, (0, 0))
+
+        # Update the display
+        pygame.display.update()
     pygame.quit()
 
 
